c9564e49368b39d4dbd8e163ad2e287734e66027
Green8888Elephant <green88elephant@gmail.com>
2023-07-04 14:05:51 +0300

    Added Header with Socials

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,28 @@
+import Image from "next/image"
+import Socials from "./Socials"
+import Link from "next/link"
+
 const Header = () => {
-    return <header className="absolute z-30 w-full">Header</header>
+    return (
+        <header className="absolute z-30 flex w-full items-center px-16 xl:h-[90px] xl:px-0">
+            <div className="container mx-auto">
+                <div className="flex flex-col content-around items-center items-center justify-between gap-y-6 py-8 lg:flex-row">
+                    {/*    logo*/}
+                    <Link href={"/"}>
+                        <Image
+                            src={"/logo.svg"}
+                            width={220}
+                            height={48}
+                            priority={true}
+                            alt=""
+                        />
+                    </Link>
+                    {/*    socials*/}
+                    <Socials />
+                </div>
+            </div>
+        </header>
+    )
 }
 
 export default Header
--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,47 @@
-const Socials = () => {
-  return <div>Socials</div>;
-};
+import Link from "next/link"
+import {
+    RiDribbbleLine,
+    RiFacebookBoxLine,
+    RiInstagramLine,
+    RiPinterestLine,
+    RiYoutubeLine,
+} from "react-icons/ri"
 
-export default Socials;
+const Socials = () => {
+    return (
+        <div className="flex items-center gap-x-5 text-lg">
+            <Link
+                href={"/"}
+                className="transition-all duration-300 hover:text-accent"
+            >
+                <RiYoutubeLine />
+            </Link>
+            <Link
+                href={"/"}
+                className="transition-all duration-300 hover:text-accent"
+            >
+                <RiInstagramLine />
+            </Link>
+            <Link
+                href={"/"}
+                className="transition-all duration-300 hover:text-accent"
+            >
+                <RiFacebookBoxLine />
+            </Link>
+            <Link
+                href={"/"}
+                className="transition-all duration-300 hover:text-accent"
+            >
+                <RiDribbbleLine />
+            </Link>
+            <Link
+                href={"/"}
+                className="transition-all duration-300 hover:text-accent"
+            >
+                <RiPinterestLine />
+            </Link>
+        </div>
+    )
+}
+
+export default Socials
