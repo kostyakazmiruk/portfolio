4d0201eb48636f346a843116264edfacae1332ad
Green8888Elephant <green88elephant@gmail.com>
2023-07-05 21:21:05 +0300

    Need to fix the issues

--- pages/about/index.js
+++ pages/about/index.js
@@ -18,6 +18,7 @@ import Circles from "../../components/Circles"
 import Avatar from "../../components/Avatar"
 import { motion } from "framer-motion"
 import { fadeIn } from "../../variants"
+import CountUp from "react-countup"
 
 const aboutData = [
     {
@@ -111,8 +112,61 @@ const About = () => {
                 <Avatar />
             </motion.div>
             <div className="container mx-auto flex h-full flex-col items-center gap-x-6 xl:flex-row">
-                <div className="flex flex-1 flex-col justify-center">text</div>
-                <div className="flex h-[480px] w-full flex-col xl:max-w-[48%]">
+                {/*text*/}
+                <div className="flex flex-1 flex-col justify-center">
+                    <motion.h2
+                        variants={fadeIn("right", 0.2)}
+                        initial="hidden"
+                        animate="show"
+                        exit="hidden"
+                        className="h2"
+                    >
+                        Captivating <span className="text-accent">stories</span>{" "}
+                        birth magnigicent designs.
+                    </motion.h2>
+                    <motion.p
+                        variants={fadeIn("right", 0.4)}
+                        initial="hidden"
+                        animate="show"
+                        exit="hidden"
+                        className="mx-auto mb-6 max-w-[500px] px-2 xl:mx-0 xl:mb-12 xl:px-0"
+                    >
+                        10 years ago, I began freelancing as a developer. Since
+                        then, I've done remote work for agenciec, consulted for
+                        startups, and collaborated on digital products for
+                        business and consumer use.
+                    </motion.p>
+                    {/*    counters*/}
+                    <motion.div
+                        variants={fadeIn("right", 0.6)}
+                        initial="hidden"
+                        animate="show"
+                        exit="hidden"
+                        className="mx-auto mb-8 hidden md:flex md:max-w-xl xl:mx-0 xl:max-w-none"
+                    >
+                        <div className="flex flex-1 xl:gap-x-6">
+                            {/*experience*/}
+                            <div className="relative flex-1 after:absolute after:right-0 after:top-0 after:h-full after:w-[1px] after:bg-white/10">
+                                <div className="mb-2 text-2xl font-extrabold text-accent xl:text-4xl">
+                                    <CountUp start={0} end={10} duration={5} />{" "}
+                                    +
+                                </div>
+                                {/*:TODO: Check out why years of experince overlap, and find why icon of avatar doesnt show as in turorial*/}
+                                <div className="text-nowrap max-w-[100px] text-xs uppercase leading-[1.4px] tracking-[1px]">
+                                    Years of experience
+                                </div>
+                            </div>
+                        </div>
+                    </motion.div>
+                </div>
+                {/*info*/}
+                <motion.div
+                    variants={fadeIn("left", 0.4)}
+                    initial="hidden"
+                    animate="show"
+                    exit="hidden"
+                    className="flex h-[480px] w-full flex-col xl:max-w-[48%]"
+                >
                     <div className="mx-auto mb-4 flex gap-x-4 xl:mx-0 xl:gap-x-8">
                         {aboutData.map((item, itemIndex) => {
                             return (
@@ -137,7 +191,9 @@ const About = () => {
                                     className="flex max-w-max flex-1 flex-col items-center gap-x-2 text-white/60 md:flex-row"
                                 >
                                     {/*    title*/}
-                                    <div>{item.title}</div>
+                                    <div className="mb-2 font-light md:mb-0">
+                                        {item.title}
+                                    </div>
                                     <div className="hidden md:flex">-</div>
                                     <div>{item.stage}</div>
                                     {/*    icons*/}
@@ -154,7 +210,7 @@ const About = () => {
                             )
                         })}
                     </div>
-                </div>
+                </motion.div>
             </div>
         </div>
     )
