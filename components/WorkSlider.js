a136e32597ddc12b5fd39beae580561dc5021be5
Green8888Elephant <green88elephant@gmail.com>
2023-07-06 23:29:47 +0300

    Added Work page with slider of works that I have done

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -1,51 +1,110 @@
 // data
+import { Swiper, SwiperSlide } from "swiper/react"
+import { FreeMode, Pagination } from "swiper"
+import { BsArrowRight } from "react-icons/bs"
+import Image from "next/image"
+
 const workSlides = {
-  slides: [
-    {
-      images: [
+    slides: [
         {
-          title: 'title',
-          path: '/thumb1.jpg',
+            images: [
+                {
+                    title: "title",
+                    path: "/thumb1.jpg",
+                },
+                {
+                    title: "title",
+                    path: "/thumb2.jpg",
+                },
+                {
+                    title: "title",
+                    path: "/thumb3.jpg",
+                },
+                {
+                    title: "title",
+                    path: "/thumb4.jpg",
+                },
+            ],
         },
         {
-          title: 'title',
-          path: '/thumb2.jpg',
+            images: [
+                {
+                    title: "title",
+                    path: "/thumb4.jpg",
+                },
+                {
+                    title: "title",
+                    path: "/thumb1.jpg",
+                },
+                {
+                    title: "title",
+                    path: "/thumb2.jpg",
+                },
+                {
+                    title: "title",
+                    path: "/thumb3.jpg",
+                },
+            ],
         },
-        {
-          title: 'title',
-          path: '/thumb3.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb4.jpg',
-        },
-      ],
-    },
-    {
-      images: [
-        {
-          title: 'title',
-          path: '/thumb4.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb1.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb2.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb3.jpg',
-        },
-      ],
-    },
-  ],
-};
+    ],
+}
 
 const WorkSlider = () => {
-  return <div>Work Slider</div>;
-};
+    return (
+        <Swiper
+            spaceBetween={10}
+            pagination={{ clickable: true }}
+            modules={[Pagination]}
+            className="h-[280px] sm:h-[480px]"
+        >
+            {workSlides.slides.map((slide, slideIndex) => {
+                return (
+                    <SwiperSlide key={slideIndex}>
+                        {/* With grid row and grid col we can modify the number that we need to show on slides*/}
+                        <div className="grid grid-cols-2 grid-rows-2 gap-4">
+                            {slide.images.map((image, imageIndex) => {
+                                return (
+                                    <div
+                                        key={imageIndex}
+                                        className="group relative flex items-center justify-center overflow-hidden rounded-lg"
+                                    >
+                                        <div className="group relative flex items-center justify-center overflow-hidden">
+                                            {/*image*/}
+                                            <Image
+                                                src={image.path}
+                                                alt=""
+                                                width={500}
+                                                height={300}
+                                            />
+                                            {/*    overlay gradient*/}
+                                            <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 transition-all duration-700 group-hover:opacity-60"></div>
+                                            {/*    title*/}
+                                            <div className="absolute bottom-0 translate-y-full transition-all duration-300 group-hover:-translate-y-10 group-hover:xl:-translate-y-20">
+                                                <div className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]">
+                                                    {/*    title part 1*/}
+                                                    <div className="delay-100">
+                                                        LIVE
+                                                    </div>
+                                                    {/*    title part 2*/}
+                                                    <div className="translate-y-[500%] transition-all delay-150 duration-300 group-hover:translate-y-0">
+                                                        PROJECT
+                                                    </div>
+                                                    {/*    icon*/}
+                                                    <div className="translate-y-[500%] text-xl transition-all delay-200 duration-300 group-hover:translate-y-0">
+                                                        <BsArrowRight />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </SwiperSlide>
+                )
+            })}
+        </Swiper>
+    )
+}
 
-export default WorkSlider;
+export default WorkSlider
--- pages/work/index.js
+++ pages/work/index.js
@@ -1,5 +1,54 @@
-const Work = () => {
-  return <div>Work</div>;
-};
+import Circles from "../../components/Circles"
+import Bulb from "../../components/Bulb"
+import ServiceSlider from "../../components/ServiceSlider"
+import { fadeIn } from "../../variants"
+import { motion } from "framer-motion"
+import WorkSlider from "../../components/WorkSlider"
 
-export default Work;
+const Work = () => {
+    return (
+        <div className="flex h-full items-center bg-primary/30 py-36">
+            <Circles />
+            <div className="container mx-auto">
+                <div className="flex flex-col gap-x-8 xl:flex-row">
+                    {/*    text*/}
+                    <div className="mb-4 flex flex-col text-center lg:text-left xl:mb-0 xl:w-[30vw]">
+                        <motion.h2
+                            variants={fadeIn("up", 0.3)}
+                            initial="hidden"
+                            animate="show"
+                            exit="hidden"
+                            className="h2 xl:mt-8"
+                        >
+                            My Work <span className="text-accent">.</span>
+                        </motion.h2>
+                        <motion.p
+                            variants={fadeIn("up", 0.4)}
+                            initial="hidden"
+                            animate="show"
+                            exit="hidden"
+                            className="maxw-[400px] mx-auto mb-4 lg:mx-0"
+                        >
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit. Dolorum, quos, voluptatum. Ducimus est ipsam
+                            neque quas sit voluptate. Autem, sit!
+                        </motion.p>
+                    </div>
+                    {/*    slider*/}
+                    <motion.div
+                        variants={fadeIn("down", 0.6)}
+                        initial="hidden"
+                        animate="show"
+                        exit="hidden"
+                        className="w-full xl:max-w-[65%]"
+                    >
+                        <WorkSlider />
+                    </motion.div>
+                </div>
+            </div>
+            <Bulb />
+        </div>
+    )
+}
+
+export default Work
