32525eb9d01f980f7c9999c378fe589c83e3c633
Green8888Elephant <green88elephant@gmail.com>
2023-08-23 16:32:59 +0300

    fix: no testimonials for now

--- components/Nav.js
+++ components/Nav.js
@@ -13,11 +13,11 @@ const links = [
     { name: "about", path: "/about", icon: <HiUser /> },
     { name: "services", path: "/services", icon: <HiRectangleGroup /> },
     { name: "work", path: "/work", icon: <HiViewColumns /> },
-    {
-        name: "testimonials",
-        path: "/testimonials",
-        icon: <HiChatBubbleBottomCenterText />,
-    },
+    // {
+    //     name: "testimonials",
+    //     path: "/testimonials",
+    //     icon: <HiChatBubbleBottomCenterText />,
+    // },
     {
         name: "contact",
         path: "/contact",
