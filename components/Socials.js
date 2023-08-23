b8829cd970f4250d87a26f3a86961b298228d422
Green8888Elephant <green88elephant@gmail.com>
2023-08-23 11:43:44 +0300

    fix: added links to my socials

--- components/Socials.js
+++ components/Socials.js
@@ -6,39 +6,40 @@ import {
     RiPinterestLine,
     RiYoutubeLine,
 } from "react-icons/ri"
+import { BsLinkedin, BsTelegram } from "react-icons/bs"
 
 const Socials = () => {
     return (
         <div className="flex items-center gap-x-5 text-lg">
+            {/*<Link*/}
+            {/*    href={"/"}*/}
+            {/*    className="transition-all duration-300 hover:text-accent"*/}
+            {/*>*/}
+            {/*    <RiYoutubeLine />*/}
+            {/*</Link>*/}
+            {/*<Link*/}
+            {/*    href={"/"}*/}
+            {/*    className="transition-all duration-300 hover:text-accent"*/}
+            {/*>*/}
+            {/*    <RiInstagramLine />*/}
+            {/*</Link>*/}
+            {/*<Link*/}
+            {/*    href={"/"}*/}
+            {/*    className="transition-all duration-300 hover:text-accent"*/}
+            {/*>*/}
+            {/*    <RiFacebookBoxLine />*/}
+            {/*</Link>*/}
             <Link
-                href={"/"}
+                href={"https://t.me/kostyakazmiruk"}
                 className="transition-all duration-300 hover:text-accent"
             >
-                <RiYoutubeLine />
+                <BsTelegram />
             </Link>
             <Link
-                href={"/"}
+                href={"https://www.linkedin.com/in/kostya-kazmiruk-7301a11b6/"}
                 className="transition-all duration-300 hover:text-accent"
             >
-                <RiInstagramLine />
-            </Link>
-            <Link
-                href={"/"}
-                className="transition-all duration-300 hover:text-accent"
-            >
-                <RiFacebookBoxLine />
-            </Link>
-            <Link
-                href={"/"}
-                className="transition-all duration-300 hover:text-accent"
-            >
-                <RiDribbbleLine />
-            </Link>
-            <Link
-                href={"/"}
-                className="transition-all duration-300 hover:text-accent"
-            >
-                <RiPinterestLine />
+                <BsLinkedin />
             </Link>
         </div>
     )
